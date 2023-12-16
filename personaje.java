fc6d8edc19789294a6dc58af56aa537c5c9ccd98
oliviatroitino <114984018+oliviatroitino@users.noreply.github.com>
2023-12-16 17:50:24 +0100

    Update personaje.java
    + enum tipo_personaje

--- personaje.java
+++ personaje.java
@@ -1,13 +1,22 @@
+enum tipo_personaje {
+    atacante,
+    defensor,
+    medico
+}
+
 public class personaje extends caracteristicas {
     private int vida;
     private int ataque;
     private int defensa;
     private elemento elemento;
     private herramienta herramienta;
+    private tipo_personaje tipo_personaje;
 
-    public personaje(int vida, int ataque, int defensa, elemento elemento, herramienta herramienta) {
+    public personaje(int vida, int ataque, int defensa, elemento elemento, herramienta herramienta,
+            tipo_personaje tipo_personaje) {
         super(vida, ataque, defensa, elemento);
         this.herramienta = herramienta;
+        this.tipo_personaje = tipo_personaje;
 
         // agrega los bonuses de la herramienta
         this.vida = this.vida + this.herramienta.getVida();
@@ -16,8 +25,12 @@ public class personaje extends caracteristicas {
     }
 
     // hereda getters de caracteristicas (vida, ataque, defensa, elemento), por lo
-    // tanto solo getter de herramienta
+    // tanto solo getter de herramienta y tipo_personaje
     public herramienta getHerramienta() {
         return herramienta;
+    }
+
+    public tipo_personaje getTipoPersonaje() {
+        return tipo_personaje;
     }
 }
